16bedeee5b63e516e45688bdd3665a59d54d90d2
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-02-11 13:24:03 -0300

    [DEL] removido senha do cliente, foi inserida a fins de teste.

--- model/Cliente.py
+++ model/Cliente.py
@@ -1,8 +1,6 @@
 import re
 from sqlalchemy import Column, String
 from services.base import Base
-import bcrypt
-
 
 class Cliente(Base):
     __tablename__ = 'clientes'
@@ -12,28 +10,15 @@ class Cliente(Base):
     nome = Column(String(100), nullable=False)
     telefone = Column(String(15), nullable=False)
     email = Column(String(100), nullable=False, unique=True)
-    senha = Column(String(75), nullable=False)
 
     def __repr__(self):
         return f"Cliente(cpf={self.cpf}, nome={self.nome}, telefone={self.telefone}, email={self.email})"
     
-    def __init__(self, cpf, nome, telefone, email, senha):
+    def __init__(self, cpf, nome, telefone, email):
         self.cpf = self.limpar_cpf(cpf)
         self.nome = nome
         self.telefone = self.formatar_telefone(telefone)
-        self.email = self.validar_email(email)
-        self.senha = self.hash_senha(senha)
-    
-    @staticmethod
-    def hash_senha(senha: str) -> str:
-        """Gera um hash seguro para a senha"""
-        salt = bcrypt.gensalt()
-        hashed_senha = bcrypt.hashpw(senha.encode(), salt)
-        return hashed_senha.decode()
-    
-    def verificar_senha(senha: str, senha_hash: str) -> bool:
-        """Verifica se a senha fornecida corresponde ao hash armazenado"""
-        return bcrypt.checkpw(senha.encode(), senha_hash.encode())
+        self.email = self.validar_email(email)    
     
     @staticmethod
     def limpar_cpf(cpf: str) -> str:
@@ -48,7 +33,6 @@ class Cliente(Base):
             raise ValueError("E-mail inválido!")
         return email
 
-    
     @staticmethod
     def formatar_telefone(telefone: str) -> str:
         """Remove pontuações e formata o número no padrão DDD + número."""
