6c610334847c632577427839a316bcb1b31f2cb9
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-02-19 20:41:13 -0300

    [IMP] Refatora método de filtragem na tela de faturamento para utilizar SQLAlchemy; adiciona métodos para calcular total e ticket médio

--- telas/TelaFaturamento.py
+++ telas/TelaFaturamento.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from sqlalchemy import text
+from sqlalchemy import and_, func, text
 from sqlalchemy.sql import text
+from model.Sacola import Sacola, SacolaProduto
 from services.FaturamentoTreeview import FaturamentoTreeview
 from services.conexao import Database
 from widgets.widgets_faturamento import create_widgets_faturamento, obter_vendedores
@@ -9,6 +10,7 @@ class TelaFaturamento(tk.Frame):
     def __init__(self, master, vendedor, role):
         super().__init__(master)
         self.master = master
+        self.con = Database().SessionLocal()
         self.vendedor = vendedor
         self.role = role
         self.vendedores = []
@@ -24,63 +26,57 @@ class TelaFaturamento(tk.Frame):
     
 
     def filtrar_dados(self):
-        # Obtendo os filtros
-        filtros = []
-        params = {}
-
-        # Filtro por Data
-        data_inicial = self.datainicial.get_date()
-        data_final = self.datafinal.get_date()
-
-        if data_inicial and data_final:
-            filtros.append("DATE(s.time_stamp) BETWEEN :data_inicial AND :data_final")
-            params['data_inicial'] = data_inicial.strftime('%Y-%m-%d')
-            params['data_final'] = data_final.strftime('%Y-%m-%d')
-
-        # Filtro por Vendedor
-        vendedor, valido_vendedor = self.filtrar_por_vendedor()
-        if valido_vendedor:
-            filtros.append("s.vendedor_usuario = :vendedor")
-            params['vendedor'] = vendedor
-
-        cpf, valido_cliente = self.filtrar_por_cliente()
-        if valido_cliente:
-            filtros.append("s.cliente_cpf = :cpf")
-            params['cpf'] = cpf
-        
-
-        sql = """
-        SELECT 
-            s.id, 
-            s.vendedor_usuario, 
-            s.cliente_cpf, 
-            COUNT(sp.sacola_id) AS 'Qts produtos', 
-            SUM(sp.total) AS 'total', 
-            s.time_stamp
-        FROM 
-            sacolas s
-        JOIN 
-            sacola_produto sp ON s.id = sp.sacola_id
-        """
-
-        if filtros:
-            sql += " WHERE " + " AND ".join(filtros)
-
-        sql += " GROUP BY s.id, s.vendedor_usuario, s.cliente_cpf, s.time_stamp"
-
         try:
-            con = Database()
-            resultados = con.encontrar_varios(sql, params)
+            query = self.con.query(
+                Sacola.id,
+                Sacola.vendedor_usuario,
+                Sacola.cliente_cpf,
+                func.count(SacolaProduto.sacola_id).label("Qts produtos"),
+                func.sum(SacolaProduto.total).label("Total"),
+                Sacola.time_stamp
+            ).join(SacolaProduto, Sacola.id == SacolaProduto.sacola_id)
+            
+            filtros = []
 
+            data_inicial = self.datainicial.get_date()
+            data_final = self.datafinal.get_date()
+            if data_inicial and data_final:
+                filtros.append(Sacola.time_stamp.between(data_inicial, data_final))
+
+            vendedor, valido_vendedor = self.filtrar_por_vendedor()
+            if valido_vendedor and vendedor != 'Todos':
+                filtros.append(Sacola.vendedor_usuario == vendedor)
+
+            cpf, valido_cliente = self.filtrar_por_cliente()
+            if valido_cliente:
+                filtros.append(Sacola.cliente_cpf == cpf)
+                
+            if filtros:
+                query = query.filter(and_(*filtros))
+            
+            query = query.group_by(Sacola.id, Sacola.vendedor_usuario, Sacola.cliente_cpf, Sacola.time_stamp)
+            resultados = query.all()
+            
+            self.filtrar_total(resultados)
+            self.filtrar_ticket_medio(resultados)
+            
+            self.txtdatainicial.delete(0, "end")
+            self.txtdatainicial.insert(0, data_inicial)
+            self.txtdatainicial.config(state="disabled")
+            self.txtdatafinal.delete(0, "end")
+            self.txtdatafinal.insert(0, data_final)
+            self.txtdatafinal.config(state="disabled")
+            
             for item in self.tree.tree.get_children():
                 self.tree.tree.delete(item)
 
             for linha in resultados:
                 self.tree.tree.insert("", "end", values=tuple(linha))
-
+                
+                    
         except Exception as e:
             print(f"Erro ao executar a consulta: {e}")
-
+            
 
     def filtrar_por_vendedor(self):
         cmbvendedor = self.cmbvendedor.get()
@@ -101,4 +97,41 @@ class TelaFaturamento(tk.Frame):
         else: 
             cpf = con.encontrar_varios('select from clientes')
             return self.clientes, False
+        
+    def filtrar_total(self, dados):
+        try:
+            total = sum(linha[4] for linha in dados if linha[4] is not None)
+
+            self.txtvalortotal.config(state="normal")
+            self.txtvalortotal.delete(0, "end")
+            self.txtvalortotal.insert(0, f"{total:.2f}")
+            self.txtvalortotal.config(state="disabled")
+        except:
+            pass
+        
+    def filtrar_ticket_medio(self, dados):
+        """Calcula o ticket médio a partir dos resultados já filtrados."""
+        try:
+            total = 0
+            total_produtos = 0
+
+            for linha in dados:
+                if linha[4] is not None:
+                    total += linha[4]
+                if linha[3] is not None:
+                    total_produtos += linha[3]
+
+            if total_produtos > 0:
+                ticket_medio = total / total_produtos
+            else:
+                ticket_medio = 0.0
+            self.txtticket.config(state="normal")
+            self.txtticket.delete(0, "end")
+            self.txtticket.insert(0, f"{ticket_medio:.2f}")
+            self.txtticket.config(state="disabled")
+
+        except Exception as e:
+            print(f"Erro ao calcular o ticket médio: {e}")
+
+    
             
